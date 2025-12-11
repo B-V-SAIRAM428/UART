d525401b22474ee954a682eae834e4fdea5fc9c8
Venkata Sairam <165484111+B-V-SAIRAM428@users.noreply.github.com>
2025-12-11 14:58:38 +0530

    Update uart_rx.v

--- uart_rx.v
+++ uart_rx.v
@@ -1,67 +1,44 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.03.2025 14:09:41
-// Design Name: 
-// Module Name: uart_rx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-module uart_rx (
-    input clk,
+module uart_tx#(parameter frequency = 100_000_000)(
+    input clk_tx,
     input rst,
-    input data_in,
-    output reg [7:0] data_out_rx,
-    output reg data_valid
+    input en,
+    input [7:0] data_in,
+    output reg data_out
 );
-
-    reg [3:0] count = 0;
-    reg [10:0] temp_data = 11'b0;
-    reg receiving = 0;
-
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            count <= 0;
-            temp_data <= 0;
-            receiving <= 0;
-            data_out_rx <= 0;
-            data_valid <= 0;
-        end else begin
-            if (!receiving && data_in == 0) begin
-                // Start bit detected
-                receiving <= 1;
-                count <= 0;
-                data_valid <= 0;
-            end else if (receiving) begin
-                if (count < 11) begin
-                    temp_data <= {data_in,temp_data[10:1]};  // LSB first
-                    count <= count + 1;
+    localparam integer baud_clk_tx = frequency/9600;
+    reg [10:0] temp =11'b1111_1111_111;
+    reg busy;
+    reg [17:0] baud_count;
+    reg [3:0] bit_cnt;
+    always @(posedge clk_tx) begin
+            if(rst) begin
+                temp <=0;
+                bit_cnt <=0;
+                busy <= 0;
+                data_out <= 1 ;
+                baud_count <= 0;
+            end else if (en && !busy) begin
+                temp[0] <= 1'b0;              // Start bit
+                temp[8:1] <= data_in;         // Data bits
+                temp[9] <= ^data_in;          // Parity bit
+                temp[10] <= 1'b1;             // Stop bit
+                busy <= 1;
+                baud_count <=0;
+            end else if (busy) begin
+                if(bit_cnt < 11) begin
+                    if(baud_count == baud_clk_tx-1'b1) begin
+                        data_out <= temp[0];
+                        temp <= {1'b1, temp[10:1]};   // Shift right
+                        baud_count <= 0;
+                        bit_cnt <= bit_cnt+1;
+                    end else 
+                        baud_count <=  baud_count +1;
                 end else begin
-                    receiving <= 0;
-                    count <= 0;
-                    // Parity check: XOR of bits [8:1] should match bit 9
-                    if ((^temp_data[7:0]) == temp_data[8]) begin
-                        data_out_rx <= temp_data[7:0];  // valid 8-bit data
-                        data_valid <= 1;
-                    end else begin
-                        data_out_rx <= 8'dx;  // parity error
-                        data_valid <= 0;
-                    end
+                    busy <= 0;
+                    bit_cnt <=0;
                 end
-            end
-        end
+            end else 
+		         data_out <= 1; 
     end
-
 endmodule
+
