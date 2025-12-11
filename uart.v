64b419103ee8596d64158bf346700648c70b0129
Venkata Sairam <165484111+B-V-SAIRAM428@users.noreply.github.com>
2025-12-11 14:59:47 +0530

    Refactor UART module to use separate clock inputs

--- uart.v
+++ uart.v
@@ -1,39 +1,16 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.03.2025 20:08:11
-// Design Name: 
-// Module Name: uart
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module uart(
-    input clk,            // System clock (e.g., 50 MHz)
+    input clk_tx,            // System clock (e.g., 50 MHz)
+    input clk_rx,
     input rst,            // Reset signal
     input en,             // Transmit enable (pulse for 1 clk)
     input [7:0] data_in,  // Byte to transmit
-    output       data_out,     // Serial TX output
     output [7:0] data_out_rx,  // Parallel RX output
-    output       data_valid    // RX data valid flag
+    output      data_valid    // RX data valid flag
 );
-
-     
+    wire data_out;     
     // UART transmitter
     uart_tx tx (
-        .clk(clk),
+        .clk_tx(clk_tx),
         .rst(rst),
         .en(en),
         .data_in(data_in),
@@ -42,13 +19,12 @@ module uart(
 
     // UART receiver
     uart_rx rx (
-        .clk(clk),
+        .clk_rx(clk_rx),
         .rst(rst),
         .data_in(data_out),
         .data_out_rx(data_out_rx),
         .data_valid(data_valid)
-    );
-
+    ); 
 endmodule
 
 
