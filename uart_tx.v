8e32662a61f056ee83e49a89e0715eee772b76d0
Venkata Sairam <165484111+B-V-SAIRAM428@users.noreply.github.com>
2025-12-11 14:59:27 +0530

    Update uart_tx.v

--- uart_tx.v
+++ uart_tx.v
@@ -1,49 +1,44 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.03.2025 11:35:27
-// Design Name: 
-// Module Name: uart_tx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-module uart_tx(
-    input clk,
+module uart_tx#(parameter frequency = 100_000_000)(
+    input clk_tx,
     input rst,
     input en,
     input [7:0] data_in,
     output reg data_out
 );
-    reg [10:0] temp ;
+    localparam integer baud_clk_tx = frequency/9600;
+    reg [10:0] temp =11'b1111_1111_111;
     reg busy;
-    always @(posedge clk ) begin
+    reg [17:0] baud_count;
+    reg [3:0] bit_cnt;
+    always @(posedge clk_tx) begin
             if(rst) begin
                 temp <=0;
-                data_out<=1;
+                bit_cnt <=0;
+                busy <= 0;
+                data_out <= 1 ;
+                baud_count <= 0;
             end else if (en && !busy) begin
                 temp[0] <= 1'b0;              // Start bit
                 temp[8:1] <= data_in;         // Data bits
                 temp[9] <= ^data_in;          // Parity bit
                 temp[10] <= 1'b1;             // Stop bit
-                busy <=1;
+                busy <= 1;
+                baud_count <=0;
             end else if (busy) begin
-                data_out <= temp[0];
-                temp <= {1'b1, temp[10:1]};   // Shift right
-                if (temp =11'b111_1111_1111)
+                if(bit_cnt < 11) begin
+                    if(baud_count == baud_clk_tx-1'b1) begin
+                        data_out <= temp[0];
+                        temp <= {1'b1, temp[10:1]};   // Shift right
+                        baud_count <= 0;
+                        bit_cnt <= bit_cnt+1;
+                    end else 
+                        baud_count <=  baud_count +1;
+                end else begin
                     busy <= 0;
-            end
+                    bit_cnt <=0;
+                end
+            end else 
+		         data_out <= 1; 
     end
 endmodule
 
